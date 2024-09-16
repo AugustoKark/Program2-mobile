856038968e2c61b6c61ec1f023c6fd7fdfa9399a
AugustoKark <augustokark@hotmail.com>
2024-09-16 19:18:06 -0300

    avanzamos login

--- KarkProject/composeApp/src/commonMain/kotlin/ar/edu/um/alumno/ui/LoginScreen.kt
+++ KarkProject/composeApp/src/commonMain/kotlin/ar/edu/um/alumno/ui/LoginScreen.kt
@@ -3,18 +3,23 @@ package ar.edu.um.alumno.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import org.jetbrains.compose.resources.painterResource
 import karkproject.composeapp.generated.resources.Res
 import karkproject.composeapp.generated.resources.header
-
-
-
 
 
 @Composable
@@ -33,16 +38,41 @@ fun LoginScreen() {
 @Composable
 fun Login(modifier: Modifier) {
     Column (modifier = modifier){
-        HeaderImage()
+        HeaderImage(modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.padding(16.dp))
+        EmailField()
     }
 
 }
 
 @Composable
-fun HeaderImage() {
+fun EmailField() {
+    TextField(
+        value = "email", onValueChange = {  },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFF636262),
+            backgroundColor = Color(0xFFDEDDDD),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+        )
+
+
+
+
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
     Image(
         painter = painterResource( Res.drawable.header),
-        contentDescription = "Header"
+        contentDescription = "Header",
+        modifier = modifier
 
     )
 }
